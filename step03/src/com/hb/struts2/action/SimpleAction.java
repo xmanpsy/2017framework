a89e5839a897c7a3affcd096462c9172632e1b63
1027 <1027@hb6012>
2017-06-01 17:25:52 +0900

    maven_struts_ibatis(데이터베이스연동)_최종

---
+++ step03/src/com/hb/struts2/action/SimpleAction.java
@@ -0,0 +1,131 @@
+package com.hb.struts2.action;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import com.hb.struts2.model.SimpleDao;
+import com.hb.struts2.model.SimpleVo;
+import com.opensymphony.xwork2.ModelDriven;
+import com.opensymphony.xwork2.Preparable;
+
+public class SimpleAction implements ModelDriven<SimpleVo>, Preparable{
+	private SimpleVo bean;
+	SimpleDao dao = new SimpleDao();
+	private static Logger log = Logger.getLogger(SimpleAction.class);
+	private List<SimpleVo> list;
+	private int sabun;
+	private String msg;
+	
+	
+	
+	public String getMsg() {
+		return msg;
+	}
+	
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	
+	
+	public SimpleVo getBean() {
+		return bean;
+	}
+	
+	public void setSabun(int sabun) {
+		this.sabun = sabun;
+	}
+	
+	
+	
+	public List<SimpleVo> getList() {
+		return list;
+	}
+
+	public void setList(List<SimpleVo> list) {
+		this.list = list;
+	}
+
+	public String execute() {
+		return "errors";
+
+	}
+
+	public String list() {
+			try {
+				list = dao.selectAll();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		return "success";
+	}
+
+	public String add() {
+		return "success";
+	}
+	public String insert(){
+		
+		try {
+			dao.insertOne(bean);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return "success";
+	}
+	public String detail() {
+		log.debug("sabun:" + sabun);
+		try {
+			bean=dao.selectOne(sabun);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return "success";
+	}
+	public String update(){
+		log.debug(bean);
+		int result=0;
+	
+		try {
+			result = dao.updateOne(bean);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		if(result>0)
+			return "success";
+		else
+			return "input";
+	}
+
+	public String delete(){
+		int result=0;
+		try {
+			result = dao.deleteOne(sabun);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		if(result>0)
+			return "success";
+		else
+			return "input";
+	}
+	
+	
+	@Override
+	public SimpleVo getModel() {
+		
+		return bean;
+	}
+
+	@Override
+	public void prepare() throws Exception {
+		bean=new SimpleVo();
+		
+	}
+	
+	
+
+}
---
+++ step03/src/com/hb/struts2/model/SimpleDao.java
@@ -0,0 +1,66 @@
+package com.hb.struts2.model;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ibatis.common.resources.Resources;
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.ibatis.sqlmap.client.SqlMapClientBuilder;
+
+public class SimpleDao {
+	
+	private static SqlMapClient smc;
+
+
+	public SimpleDao() {
+		String path="./SqlMapConfig.xml";
+		try {
+			Reader reader=Resources.getResourceAsReader(path);
+			smc=SqlMapClientBuilder.buildSqlMapClient(reader);
+			
+			
+			
+			
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	public List<SimpleVo> selectAll() throws SQLException {
+		return smc.queryForList("selectAll");
+	}
+
+
+	public SimpleVo selectOne(int sabun) throws SQLException {
+		
+		
+		return (SimpleVo) smc.queryForObject("selectOne", sabun);
+	}
+
+
+	public int updateOne(SimpleVo bean) throws SQLException {
+		return smc.update("updateOne", bean);
+	}
+
+
+	public void insertOne(SimpleVo bean) throws SQLException {
+		smc.insert("insertOne", bean);
+		
+	}
+
+
+	public int deleteOne(int sabun) throws SQLException {
+		
+		return smc.delete("deleteOne",sabun);
+	}
+	
+
+	
+}
